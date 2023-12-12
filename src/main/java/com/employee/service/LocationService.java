038837ad80ed8bbf1c43c6b96fb632ed73799fcc
Shubham Waychal <swaychal6@gmail.com>
2023-12-12 20:16:58 +0530

    addes some changes to show the mesageto check the working in repo

--- src/main/java/com/employee/service/LocationService.java
+++ src/main/java/com/employee/service/LocationService.java
@@ -8,4 +8,8 @@ public class LocationService {
 	public void showDefaultMessage() {
 		System.out.println("Putting message here for checkig git bash changes!!!");
 	}
+	
+	public void show() {
+		System.out.println("jhsbbqhxb");
+	}
 }
